23f1cc2d6a3d0869e975bf6b6d55d432e05b4d92
michbur <michalburdukiewicz@gmail.com>
2024-09-04 22:43:36 +0200

    clearing the code

--- setup-environment.R
+++ setup-environment.R
@@ -1,4 +1,3 @@
 install.packages(c("rmarkdown", "tidyverse", "DT", "slickR", "RCurl", "openalexR",
-                   "leaflet", "icons", "hrbrthemes", "ggalt", "ggbeeswarm", "remotes"))
-remotes::install_github("mitchelloharawild/icons")
-icons::download_fontawesome()
+                   "leaflet", "icons", "hrbrthemes", "ggalt", "ggbeeswarm", "fontawesome"))
+
