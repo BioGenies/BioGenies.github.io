4679943c1ab140eed5ee158318f158f17bcf0147
jarochi <jaroslaw.chilimoniuk@gmail.com>
2024-11-14 12:17:00 +0100

    rm wordcloud2

--- setup-environment.R
+++ setup-environment.R
@@ -1,4 +1,3 @@
 install.packages(c("rmarkdown", "tidyverse", "DT", "slickR", "RCurl", "openalexR",
-                   "leaflet", "fontawesome", "hrbrthemes", "ggalt", "ggbeeswarm", "wordcloud2",
-                   "curl"))
+                   "leaflet", "fontawesome", "hrbrthemes", "ggalt", "ggbeeswarm"))
 
