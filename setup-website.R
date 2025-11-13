2a5cf6477b8ffc906a2acac3bcbb51ced49f61d2
jarochi <jaroslaw.chilimoniuk@gmail.com>
2025-11-13 14:48:58 +0100

    updates

--- setup-website.R
+++ setup-website.R
@@ -1,12 +1,12 @@
 system("quarto render")
 
-files <- list.files(pattern = ".qmd$", recursive = TRUE)
-files <- files[!grepl("_temp.qmd|fun.qmd", files)]
+# files <- list.files(pattern = ".qmd$", recursive = TRUE)
+# files <- files[!grepl("_temp.qmd|fun.qmd", files)]
 
-for (f in files) {
-  rmarkdown::render(f, output_format = "all")
-}
+# for (f in files) {
+#   rmarkdown::render(f, output_format = "all", output_dir = "docs")
+# }
 
-for (file in files) {
-  rstudioapi::executeCommand("knitDocument", list(path = file))
-}
+# for (file in files) {
+#   rstudioapi::executeCommand("knitDocument", list(path = file))
+# }
